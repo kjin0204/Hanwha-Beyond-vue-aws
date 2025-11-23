00ba8b71364e91de839e9f2d4dcb6feb2bf7ac7a
hyeongKang <kin0204@naver.com>
2025-11-23 11:34:17 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  function move() {
+    location.href="https://naver.com"
+  }
+
+</script>
+
+<template>
+  <div>
+    <button v-on:click="move">무브무브</button>
+  </div>
+</template>
+
+
+
+<style scoped>
+</style>
